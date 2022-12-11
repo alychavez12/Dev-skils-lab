2d5eff030bce2bf4c88498b56f580df32cb15b32
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2022-12-11 11:31:25 -0500

    feat: add buttom input and click to removes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 
 
+// add a elements to the list when the button is click.
+$('button').click(function() {
+    let mylist = $('#input').val();
+    $('#list').append('<li>'+mylist+'</li>');
+    return false;   
+ });
+
+ //it will remove skills from the list with one click
+
+ $('ul').on('click', 'li', function() {
+    $(this).remove();
+ })
+
+
+
 
