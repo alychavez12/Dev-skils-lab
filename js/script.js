c41a922e1339373c70b29f097dec1b10e7e3b309
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2022-12-12 11:43:10 -0500

    feat: adds redIcon to js and html.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 
 
+let redIcon = "<i id='icon' class='fas fa-times'></i>";
+
 // add a elements to the list when the button is click.
 $('button').click(function() {
     let mylist = $('#input').val();
-    $('#list').append('<li>'+mylist+'</li>');
+    $('#list').append(`<li>${redIcon} ${mylist}</li>`);
     return false;   
  });
 
- //it will remove skills from the list with one click
+
+ //it will remove an skill from the list with one click.
 
  $('ul').on('click', 'li', function() {
     $(this).remove();
